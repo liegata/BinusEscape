e25778828749fb9393c6cb845ef8672343f56d34
Mahesh <36764958+maheshputra@users.noreply.github.com>
2018-11-02 18:29:18 +0700

    Camera

---
+++ Assets/scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position;
+	}
+}
